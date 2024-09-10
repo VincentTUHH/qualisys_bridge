b24b8a6049dd941cf783c3f6fa35ee917a8f508f
Thies Lennart Alff <lennart.alff@gmail.com>
2024-09-10 11:43:44 +0200

    removed now erroneous namespace from launch file

--- launch/qualisys_bridge.launch.py
+++ launch/qualisys_bridge.launch.py
@@ -1,26 +1,26 @@
 from ament_index_python.packages import get_package_share_path
+from launch_ros.actions import Node
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 
 
 def declare_launch_args(launch_description: LaunchDescription):
-    action = DeclareLaunchArgument(name='vehicle_name')
-    launch_description.add_action(action)
-
     default = get_package_share_path('qualisys_bridge') / 'config/bridge.yaml'
-    action = DeclareLaunchArgument(name='bridge_config_file',
-                                   default_value=str(default))
+    action = DeclareLaunchArgument(
+        name='bridge_config_file', default_value=str(default)
+    )
     launch_description.add_action(action)
 
 
 def add_node(launch_description: LaunchDescription):
-    action = Node(package='qualisys_bridge',
-                  executable='bridge_node',
-                  name='qualisys',
-                  namespace=LaunchConfiguration('vehicle_name'),
-                  parameters=[LaunchConfiguration('bridge_config_file')])
+    action = Node(
+        package='qualisys_bridge',
+        executable='bridge_node',
+        name='qualisys',
+        parameters=[LaunchConfiguration('bridge_config_file')],
+    )
     launch_description.add_action(action)
 
 
